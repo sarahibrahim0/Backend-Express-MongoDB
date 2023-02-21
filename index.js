6743b64b60ab63640002a983e71e3d10688c3fbb
Sarah Ibrahim <75214911+sarahibrahim0@users.noreply.github.com>
2023-02-21 13:42:50 +0200

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ const session = require('express-session');
 
 const parseurl = require('parseurl');
 
-const port = 3000;
 
 
 
@@ -100,7 +99,7 @@ mongoose
   .connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log("Database Connection is ready.");
-app.listen(port, ()=>{
+app.listen(`0.0.0.0:$PORT`, ()=>{
   console.log('server is runnin now on 3000')
 })
 
