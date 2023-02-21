d0e94686dee9df60d63530d4584849192d508085
sarah ibrahim <sarahibrahimabelhamid@gmail.com>
2023-02-21 23:13:23 +0200

    gjgj

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express');
-
 const app = express();
 
 const morgan = require('morgan');
 
 const mongoose = require('mongoose');
+
+const session = require('express-session');
+
+const parseurl = require('parseurl');
+
 
 // Enable express to parse body data from raw application/json dat
 
@@ -14,6 +18,7 @@ app.options('*', cors())
 
 //environment  variables
 require('dotenv/config');
+const api = process.env.API_URL;
 
 
 //helpers
@@ -32,17 +37,60 @@ app.use(errHandler);
 
 app.use('/public/uploads',express.static(__dirname+'/public/uploads'));
 
+
+// Session Setup
+
+
+
+
 //routes
 const productsRouter = require('./routers/products')
 const categoriesRouter = require('./routers/categories')
 const usersRouter = require('./routers/users')
 const ordersRouter = require('./routers/orders')
+// const sessionRouter = require('./routers/session')
+
 
 //routers
-app.use(`/api/v1/products`, productsRouter);
-app.use(`/api/v1/categories`, categoriesRouter);
-app.use(`/api/v1/users`, usersRouter);
-app.use(`/api/v1/orders`, ordersRouter);
+app.use(`${api}/products`, productsRouter);
+app.use(`${api}/categories`, categoriesRouter);
+app.use(`${api}/users`, usersRouter);
+app.use(`${api}/orders`, ordersRouter);
+
+
+
+
+
+
+
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true
+}))
+
+app.use(function (req, res, next) {
+  if (!req.session.views) {
+    req.session.views = {}
+  }
+
+  // get the url pathname
+  var pathname = parseurl(req).pathname
+
+  // count the views
+  req.session.views[pathname] = (req.session.views[pathname] || 0) + 1
+
+  next();
+})
+
+app.get('/api/v1/session', function (req, res, next) {
+  console.log(req.session.views)
+  res.status(200).json({success: true , message:  req.session.views['/api/v1/session'] });
+})
+
+
+
+
 
 
 //Database
