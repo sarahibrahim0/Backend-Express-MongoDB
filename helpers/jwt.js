e1720e484bf7402c3987b52526af6607ec957355
sarah ibrahim <sarahibrahimabdelhamid@gmail.com>
2023-03-02 08:34:41 +0200

    Dd

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -13,7 +13,8 @@ function authJwt() {
       { url: /api\/v1\/products(.*)/, methods: ["GET", "OPTIONS"] },
       { url: /api\/v1\/categories(.*)/, methods: ["GET", "OPTIONS"] },
       { url: /api\/v1\/orders(.*)/, methods: ["GET", "OPTIONS"] },
-      { url: /api\/v1\/session(.*)/, methods: ["GET", "OPTIONS"] },
+      { url: /api\/v1\/users(.*)/, methods: ["GET", "OPTIONS"] },
+
 
 
 
